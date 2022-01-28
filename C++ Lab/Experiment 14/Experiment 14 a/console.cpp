3da09e522392c5626f8f0a8bce814d0af8e8dcea
renusreeja <93851701+renusreeja@users.noreply.github.com>
2022-01-28 17:26:44 +0530

    Add files via upload

---
+++ C++ Lab/Experiment 14/Experiment 14 a/console.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+	char x; 
+	// used to scan a single char 
+cout<<"Enter the character:";
+	cin.get(x); 
+	// used to put a single char onto the screen. 
+cout << "The given Character is:";	
+cout.put(x); 
+// ncount character from array 
+	cout.write("\nWelcome to Hyderabad", 5); 
+
+} 
